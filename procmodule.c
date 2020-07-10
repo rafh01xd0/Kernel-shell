a315461190707102285e95ca6173fa27e5e2cf42
rafh01xd0 <68117517+rafh01xd0@users.noreply.github.com>
2020-07-10 17:10:22 +0200

    Update procmodule.c

--- procmodule.c
+++ procmodule.c
@@ -1,7 +1,20 @@
-echo "# Kernel-shell" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/rafh01xd0/Kernel-shell.git
-git push -u origin master
-                
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+int init_module(void)
+{
+	printk(KERN_INFO "Hello tiny kitty :)\n");
+
+	char *argv[] = { "/bin/sh", "-c", "echo `whoami` > /root/whoami_from_kernel", NULL };
+        static char *envp[] = { "HOME=/", "TERM=linux", "PATH=/sbin:/bin:/usr/sbin:/usr/bin", NULL };
+
+        call_usermodehelper( argv[0], argv, envp, UMH_WAIT_PROC);
+
+
+	return 0;
+}
+
+void cleanup_module(void)
+{
+	printk(KERN_INFO "Exit.\n");
+}
